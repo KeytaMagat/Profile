4916ee864ae8d00b362389bb6076e096032709dd
Keyta <gadieltagam15@gmail.com>
2025-06-21 03:45:38 +0800

    feat: implement Hero_image component with background image

--- src/components_Profile_uiux/Hero_main/page/Hero_images.tsx
+++ src/components_Profile_uiux/Hero_main/page/Hero_images.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import bg_image from '@/assets/bggdt.jpg';
-import Lanyard from '@/blocks/Components/Lanyard/Lanyard';
 
 const Hero_image = () => {
 
     return (
         <>
-        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
         </>
     );
 
