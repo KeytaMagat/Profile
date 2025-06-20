7f137ee0c74808ac6c9351c72ebe803c8172a4a2
Keyta <gadieltagam15@gmail.com>
2025-06-21 04:26:30 +0800

    feat: add GlassIcons component for enhanced social link display

--- src/components_Profile_uiux/Hero_main/common/Social_Links.tsx
+++ src/components_Profile_uiux/Hero_main/common/Social_Links.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Github, Linkedin, Mail, Download } from 'lucide-react';
 import { profileData } from "@/data/profileData";
+import GlassIcons from "@/blocks/Components/GlassIcons/GlassIcons";
 
 const Social_Links = () => {
 
+        // update with your own icons and colors
+        const items = [
+        { icon: <Github />, color: 'red', label: 'Github' },
+        { icon: <Linkedin />, color: 'red', label: 'Linkedin' },
+        { icon: <Mail />, color: 'red', label: 'GMail' },
+        // { icon: <FiCloud />, color: 'indigo', label: 'Weather' },
+        // { icon: <FiEdit />, color: 'orange', label: 'Notes' },
+        // { icon: <FiBarChart2 />, color: 'green', label: 'Stats' },
+        ];
     return (
         <div className="w-full h-fit flex bottom-30 right-50 justify-between">
-            <a
+            {/* <a
               href={profileData.social.github}
               target="_blank"
               rel="noopener noreferrer"
@@ -27,8 +37,11 @@ const Social_Links = () => {
               className="text-gray-600 hover:text-gray-900 transition-colors"
             >
               <Mail className="h-6 w-6" />
-            </a>
-          </div>
+            </a> */}
+            
+            <GlassIcons items={items} className="custom-class"/>
+            
+        </div>
     );
 };
 
